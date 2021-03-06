bdfa48f55c6b4ec1f0db2dbd2f47203546c82a21
LiangHao <liangshuai@email.swu.edu.cn>
2021-03-06 16:31:29 +0800

    抢座V1.2：在v1.1的基础之上，加入了控制台日志，即在控制台显示状态和操作。

--- choose.py
+++ choose.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import time
+import datetime
 import smtplib
 from email.mime.text import MIMEText
-#供调用,返回结果
+#供调用,返回结果(提示，所选分钟)
 def login(url,username,password):
+    re = [];
     broser = webdriver.Chrome();  # 实例化浏览器
     broser.get(url);#模拟登录
     time.sleep(2);#睡眠2s
@@ -15,17 +17,29 @@ def login(url,username,password):
     #在这里要检验一下：
     temp_msg=broser.find_element_by_xpath("/html/body/div/div/p");
     if temp_msg.text == "操作失败":
-        temp_msg_1=broser.find_element_by_xpath("/html/body/div/div[2]/p");
+        temp_msg_1=broser.find_element_by_xpath("/html/body/div/div[2]/p");#查看失败原因
         #sendEmail(temp_msg_1.text,"2609362670@qq.com");
-        return temp_msg_1.text;
+        re.append(temp_msg_1.text);
+        re.append(-1)
+        return re;
     options = broser.find_elements_by_xpath("/html/body/div/form/div/p[3]/select/option");
     length=len(options);#查看当前有多少可选项
     #优先选择最长的
+    strs=options[length-1].text;
+    ti = ""
+    for i in range(0, len(strs)):
+        if strs[i] != '分':
+            ti += strs[i];
+        else:
+            break;
     options[length-1].click();
     time.sleep(2);
     broser.find_element_by_xpath("/html/body/div/form/div[2]/button").click();
     result = broser.find_element_by_xpath("/html/body/div/div/p");#操作后的结果
-    return result.text;
+    print(result.text)
+    re.append(result.text);
+    re.append(int(ti))
+    return re;
 #发送通知邮件
 def sendEmail(text,remail):
     smtp = "smtp.163.com";
@@ -54,17 +68,61 @@ def selectSeat():
     user_1="222018603193001";
     pwd_1="Lianghao.215";
     text_0=login(url_1,user_1,pwd_1)
-    if text_0 == "不在开放时间":
-        sendEmail("占座失败"+text_0,remail);
+    if text_0[1] <=0:
+        sendEmail("占座失败"+text_0[0],remail);
         return -1;
-    text_1="2F-001 "+text_0;
+    text_1="2F-001 "+text_0[0];
     sendEmail(text_1,remail);
     time.sleep(10);
     url_2="http://update.unifound.net/wxnotice/s.aspx?c=100601185_Seat_100601457_1FC";
     user_2 = "222018603193015";
     pwd_2 = "222018603193015";
-    text_2 = "2F-002 "+login(url_2,user_2,pwd_2);
-    sendEmail(text_2,remail);
-    return 1;
+    text_2 = login(url_2, user_2, pwd_2)
+    print(text_2[0])
+    #if text_2[1] == "不在开放时间" or text_2[0] =="设备已分配给他人使用,未暂离":
+    if text_2[1]<=0:
+        sendEmail("占座失败:" + text_2[0], remail);
+        return -1;
+    text_3 = "2F-002 " + text_2[0];
+    sendEmail(text_3, remail);
+    return text_2[1];
+#通用的选择函数，暂不使用
+def selectSeat_(url,user,pwd,seat):
+    remail = "2609362670@qq.com";
+    text_0=login(url,user,pwd)
+    if text_0[1] <=0:
+        sendEmail("占座失败"+text_0[0],remail);
+        return -1;
+    text_1=seat+" "+text_0[0];
+    sendEmail(text_1,remail);
+    time.sleep(10);
+    return text_1[1];
+#计算时间的函数
+def nowTime():
+    dt = datetime.datetime.now();
+    hours=dt.hour;#小时[0,24]
+    mins=dt.minute;#分钟[0,60]
+    return hours*60+mins;
 if __name__ == '__main__':
-    selectSeat()
+    i=1;
+    s=0;
+    f=0;
+    fs=0;
+    while True:
+        print("{0}:第{1}次运行，成功占座:{2}次，失败{3}次，无效时间内运行{4}次".format(datetime.datetime.now(),i,s,f,fs))
+        if(nowTime()>=480 and nowTime()<=1260):
+            times=selectSeat();#返回所选时间
+            if(times>=0):
+                print(datetime.datetime.now(),"-----","已经选择:",times,"分钟");
+                print("当前时间为:",int(nowTime()/60),":",nowTime()%60)
+                s=s+1
+                time.sleep(times+10);
+            else:
+                print("------作为已经被占用，下一次尝试占座时间为:", int(nowTime() / 60) + 1, ":", nowTime() % 60)
+                time.sleep(3600);#休眠1个小时
+                f = f + 1;
+        elif nowTime()<400 or nowTime()>1300:
+            print("------不在开放时间，即将休眠，下一次尝试占座时间为:",int(nowTime()/60)+1,":",nowTime()%60)
+            fs=fs+1
+            time.sleep(3600);
+        i=1+1;
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-This is update
+'''
+实现了登录西南大学教务系统
+'''
+from selenium import webdriver
+import requests
+import time
+from selenium.webdriver.common.keys import Keys
+broser=webdriver.Chrome();#实例化浏览器
+#broser.get("https://uaaap.swu.edu.cn/cas/login");
+broser.get("https://uaaap.swu.edu.cn/cas/login?service="
+           "http://i.swu.edu.cn/PersonalApplications/viewPageV3");
+time.sleep(2);
+cookie_bro=broser.get_cookies();
+cookie1=cookie_bro[0]['value'];
+print("当前cookie:"+cookie1)
+broser.find_element_by_id("username").send_keys("liangshuai")
+broser.find_element_by_id("password").send_keys("Lianghao.215")
+broser.find_element_by_class_name("blue").click()
+###登录成功
+time.sleep(2);
+time.sleep(1)
+#search_window = broser.current_window_handle  # 当登陆后有新增一个网页，用此代码来定位
+
+#根据a标签的全部文本
+##一定要注意以下两个语句的区别，被注释的语句是错误的
+#即用xpath寻找元素是：driver.find_element。只能寻找一个
+#broser.find_elements_by_xpath("/html/body/div[2]/div/div/div/div/div[2]/div/a[3]");
+#登录教务系统
+broser.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div[2]/div/a[3]").click();
+time.sleep(2);#登录成功
